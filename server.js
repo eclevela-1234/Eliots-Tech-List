febb6114df5ebb08cd33055daff9516bfb511229
Eliot Cleveland <eclevela@richmond.edu>
2022-10-25 23:27:21 -0400

    edit posts works

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    expires: 60000
+    expires: 600000
   },
   resave: false,
   saveUninitialized: false,
