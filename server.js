365e910037932976e06d72da07f10700ff8f9a98
Eliot Cleveland <eclevela@richmond.edu>
2022-10-25 21:53:04 -0400

    updates to styling

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    expires: 60000
+  },
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: new SequelizeStore({
     db: sequelize
   })
